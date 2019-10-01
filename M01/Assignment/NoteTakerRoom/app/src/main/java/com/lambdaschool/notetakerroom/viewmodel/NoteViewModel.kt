29101c7453a64641befe57e222ff61cf19d17f42
jack seymour <stilljack@gmail.com>
2019-10-01 14:05:16 -0700

    //01 super basic super sketchy reorganization because i can't freakin look at that unsorted.
    //02 start at view model... the heck
    
    //03 there are less bugs in the viewmodel so much as it's just completely bad top to bottom, going to try to take it in this direction

--- M01/Assignment/NoteTakerRoom/app/src/main/java/com/lambdaschool/notetakerroom/viewmodel/NoteViewModel.kt
+++ M01/Assignment/NoteTakerRoom/app/src/main/java/com/lambdaschool/notetakerroom/viewmodel/NoteViewModel.kt
@@ -1,29 +1,40 @@
 package com.lambdaschool.notetaker
 
+import android.app.Application
 import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 import java.util.ArrayList
 
-class NoteViewModel : ViewModel() {
-    private var noteList: MutableLiveData<ArrayList<Note>>? = null
+class NoteViewModel(application:Application) : AndroidViewModel(application) {
+  //if you want context in a viewMode, this is how we do it
+  val context:Context =application.applicationContext
+    //changed to by lazt
+
+    private var noteList: MutableLiveData<ArrayList<Note>> by lazy {
+        getNotesList()
+    }
     private var repo: NoteRepository? = null
 
-    fun getNotesList(context: Context): LiveData<ArrayList<Note>>? {
+    //signatures wrong
+    fun getNotesList(): MutableLiveData<ArrayList<Note>> {
         if (noteList == null) {
-            loadList(context)
+            loadList()
         }
         return noteList
     }
 
-    private fun loadList(context: Context) {
+    private fun loadList() {
         repo = NoteRepository(context)
         noteList = repo!!.getNotes(context)
     }
 
-    fun addNote(note: Note, context: Context) {
+    fun addNote(note: Note) {
+        //so add notes shouldn't need to mess with note list as we are using live data
+        //I think -- for now we
         if (noteList != null) {
             repo!!.addNote(note)
             noteList = repo.getNotes(context);
