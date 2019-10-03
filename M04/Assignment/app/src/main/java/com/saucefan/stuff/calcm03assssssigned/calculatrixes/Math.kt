9ec12e4a46c17a1a3e3918b82fd7cc0b59109609
jack seymour <stilljack@gmail.com>
2019-10-03 13:25:33 -0700

    init UI

---
+++ M04/Assignment/app/src/main/java/com/saucefan/stuff/calcm03assssssigned/calculatrixes/Math.kt
@@ -0,0 +1,51 @@
+package com.saucefan.stuff.calcm03assssssigned.calculatrixes
+
+import kotlin.math.absoluteValue
+
+class Maths() {
+
+
+    fun getSquareSum(x: Int, y: Int): Int {
+
+        //the sum of two numbers squared
+        //or
+        //"calculate the sum of squares of two numbers"
+        return (x * x) + (y * y)
+
+    }
+
+    fun factorial(x: Int): Int {
+        var sum = x
+        if (x > 0) {
+            for (i in x-1 downTo 1) {
+                sum *= i
+
+            }
+        }
+        return sum
+    }
+
+    fun diffAbsValue(x: Int, y: Int): Int {
+        val absX = x.absoluteValue
+        val absY = y.absoluteValue
+        if (absX >= absY) {
+            return absX - absY
+        }
+        return absY - absX
+    }
+
+    fun palidrome(string: String): Boolean {
+            val string = string.toCharArray()
+            val reverse = string.reversed().toCharArray()
+            for (i in string.indices) {
+                if (string[i] != reverse[i]) {
+                    return false
+                }
+            }
+            return true
+
+        }
+
+
+
+}
